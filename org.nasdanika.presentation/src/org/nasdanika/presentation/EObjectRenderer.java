2567920f367b07dea79eef5914b41253b5b4bdbb
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2017-08-31 12:52:42 -0400

    Util.render() encapsulates registration lookup and EMF Forms fallback

--- org.nasdanika.presentation/src/org/nasdanika/presentation/EObjectRenderer.java
+++ org.nasdanika.presentation/src/org/nasdanika/presentation/EObjectRenderer.java
@@ -1,6 +1,9 @@
 package org.nasdanika.presentation;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecp.ui.view.swt.ECPSWTViewRenderer;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.swt.widgets.Composite;
 
@@ -12,6 +15,35 @@ import org.eclipse.swt.widgets.Composite;
 public interface EObjectRenderer {
 	
 	/**
+	 * Helper class.
+	 * @author Pavel Vlasov
+	 *
+	 */
+	class Util {
+		
+		/**
+		 * Renders EObject using either a registered {@link EObjectRenderer} or EMF Forms {@link ECPSWTViewRenderer}, if there is no EObjectRenderer registration.
+		 * @param parent
+		 * @param eObject
+		 * @param editingDomain
+		 * @throws Exception
+		 */
+		public static void render(Composite parent, EObject eObject, EditingDomain editingDomain) throws Exception {
+			for (IConfigurationElement ce: Platform.getExtensionRegistry().getConfigurationElementsFor("org.nasdanika.presentation.eobject_renderer")) {
+				// TODO renderers cache to improve performance?
+				if ("eobject_renderer".equals(ce.getName()) 
+						&& eObject.eClass().getName().equals(ce.getAttribute("eclass_name"))
+						&& eObject.eClass().getEPackage().getNsURI().equals(ce.getAttribute("epackage_ns_uri"))) {
+					((EObjectRenderer) ce.createExecutableExtension("renderer_class_name")).render(parent, eObject, editingDomain);
+					return;
+				}
+			}		
+			ECPSWTViewRenderer.INSTANCE.render(parent, eObject);
+		}
+		
+	}
+	
+	/**
 	 * Renders UI for editing EObject data.
 	 * @param parent Parent composite.
 	 * @param eObject {@link EObject} to render UI for.
@@ -19,5 +51,5 @@ public interface EObjectRenderer {
 	 * @throws Exception
 	 */
 	void render(Composite parent, EObject eObject, EditingDomain editingDomain) throws Exception;
-
+	
 }
